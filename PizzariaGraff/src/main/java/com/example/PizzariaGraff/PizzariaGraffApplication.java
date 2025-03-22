dd9ce32b9d83718f24f002a021ac156b9865c755
Dallacort <matheusdallacort04@gmail.com>
2025-03-22 20:04:39 -0300

    primeiros dia do projeto

---
+++ PizzariaGraff/src/main/java/com/example/PizzariaGraff/PizzariaGraffApplication.java
@@ -0,0 +1,13 @@
+package com.example.PizzariaGraff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariaGraffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariaGraffApplication.class, args);
+	}
+
+}
---
+++ PizzariaGraff/src/test/java/com/example/PizzariaGraff/PizzariaGraffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PizzariaGraff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariaGraffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
